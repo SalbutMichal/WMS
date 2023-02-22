0b8a162468b3d05730535c1485d1dd6b53129fd1
SalbutMichal <salbutmichal98@gmail.com>
2023-02-22 22:05:15 +0100

    Struktóra routingów oraz podstawowe widoki

---
+++ resources/views/dashboard/products/create_product.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.dashboard')
+@section('nav')
+<div class="app-header-navigation">
+    <div class="tabs">
+        <a href="#" class="active">
+            Lista produktów
+        </a>
+        <a href="#">
+            Dodaj nowy
+        </a>
+    </div>
+</div>
+@endsection
+@section('content')
+
+@endsection
---
+++ resources/views/dashboard/warehouse/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.dashboard')
+@section('nav')
+<div class="app-header-navigation">
+    <div class="tabs">
+        <a href="#" class="active">
+            Lista magazynów
+        </a>
+        <a href="#" >
+            Przyjęcia
+        </a>
+        <a href="#">
+            Wydania
+        </a>
+        <a href="#">
+            Utwórz nowy
+        </a>
+    </div>
+</div>
+@endsection
+@section('content')
+
+@endsection
